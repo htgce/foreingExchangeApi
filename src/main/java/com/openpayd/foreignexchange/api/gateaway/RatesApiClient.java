15931d9f82d278561f10100faad52cac285c7b0f
htugce <tugce.ozkaptan@gmail.com>
2020-11-08 18:26:59 +0300

    FeingClient interface method added.

--- src/main/java/com/openpayd/foreignexchange/api/gateaway/RatesApiClient.java
+++ src/main/java/com/openpayd/foreignexchange/api/gateaway/RatesApiClient.java
@@ -4,13 +4,17 @@ import com.openpayd.foreignexchange.api.gateaway.response.RatesApiResponse;
 import feign.Param;
 import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Currency;
 import java.util.List;
 
-@FeignClient(value = "rates-service", url = "https://ticketservice.com/")
+@FeignClient(value = "rates-service", url = "https://api.ratesapi.io/api/latest")
 public interface RatesApiClient {
-    @RequestLine("GET {path}?base={base}&symbols={symbols}")
-    RatesApiResponse getRates(@Param("path") String path, @Param("base") Currency base,
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    RatesApiResponse getRates(@Param("base") Currency base,
                               @Param("symbols") List<Currency> symbols);
+
 }
