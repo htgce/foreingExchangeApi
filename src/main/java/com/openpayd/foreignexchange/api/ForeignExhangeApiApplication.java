20b29fdfd268a8debad6d90dea515a0e552f96d7
htugce <tugce.ozkaptan@gmail.com>
2020-11-08 17:25:29 +0300

    Initial commit

---
+++ src/main/java/com/openpayd/foreignexchange/api/ForeignExhangeApiApplication.java
@@ -0,0 +1,13 @@
+package com.openpayd.foreignexchange.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignExhangeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForeignExhangeApiApplication.class, args);
+    }
+
+}
