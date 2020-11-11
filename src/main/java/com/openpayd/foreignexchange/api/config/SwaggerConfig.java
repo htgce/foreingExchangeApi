8d85e06e5e379a7096143d345730189f246cb289
htugce <tugce.ozkaptan@gmail.com>
2020-11-11 03:33:24 +0300

    Swagger added.

--- src/main/java/com/openpayd/foreignexchange/api/config/SwaggerConfig.java
+++ src/main/java/com/openpayd/foreignexchange/api/config/SwaggerConfig.java
@@ -1,19 +1,13 @@
 package com.openpayd.foreignexchange.api.config;
 
 import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 
 @Configuration
 @AllArgsConstructor
 public class SwaggerConfig {
-
+/*
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -21,6 +15,6 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build().apiInfo(ApiInfo.DEFAULT);
-    }
+    }*/
 
 }
